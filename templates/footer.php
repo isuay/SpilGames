f97a26e70a99160ff5860d7cb394c85607add5e8
AlexisPerez445 <traceur35@gmail.com>
2022-05-11 20:22:39 +0200

    templates

---
+++ templates/footer.php
@@ -0,0 +1,49 @@
+</body>
+
+<footer class="row row-cols-3 py-4 border-top text-center mx-5">
+    <div class="col">
+        <img id="logo-footer" src="img/logos/logo.png" alt="Logo">
+    </div>
+
+    <div class="col">
+        <h5>Juegos</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        </ul>
+    </div>
+
+    <div class="col">
+        <h5>Contactos</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        </ul>
+    </div>
+</footer>
+
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top mx-5">
+    <div class="col-md-4 d-flex align-items-center">
+        <span class="text-muted">&copy; 2021 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3">
+            <a href="https://twitter.com/?lang=es" target="_blank">
+                <i class="fa-brands fa-twitter text-dark"></i>
+            </a>
+        </li>
+        <li class="ms-3">
+            <a href="https://www.instagram.com/" target="_blank">
+                <i class="fa-brands fa-instagram text-dark"></i>
+            </a>
+        </li>
+        <li class="ms-3">
+            <a href="https://es-es.facebook.com/" target="_blank">
+                <i class="fa-brands fa-facebook text-dark"></i>
+            </a>
+        </li>
+    </ul>
+</footer>
+
+</html>
--- templates/header.php
+++ templates/header.php
@@ -8,21 +8,16 @@
     <meta name="description" content="HTML5, CSS3">
     <meta name="author" content="">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <header class="d-flex flex-wrap justify-content-between py-3 mb-4 mx-5 border-bottom ">
-        <img id="logo-web" src="img/logo.png" alt="Logo">
+        <img id="logo-web" src="img/logos/logo.png" alt="Logo">
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container" id="main_nav">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32">
-                        <use xlink:href="#bootstrap" />
-                    </svg>
-                </a>
-
                 <ul class="nav nav-pills" id="menu-links">
                     <li class="nav-item"><a href="#" class="nav-link" aria-current="page">Home</a></li>
                     <li class="nav-item dropdown">
