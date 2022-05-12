41b67c7f6855c23c341fd38dd136ba1d237448b2
isuay <irisau2@gmail.com>
2022-05-12 19:25:35 +0200

    flex

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 </body>
 
-<footer class="row row-cols-3 py-4 text-center" id="f1">
+<footer class="row row-cols-3 py-4 text-center border-bottom" id="f1">
     <div class="col">
         <img id="logo-web" src="img/logos/logo.png" alt="Logo">
     </div>
@@ -22,28 +22,30 @@
     </div>
 </footer>
 
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top contenedor" id="f2">
-    <div class="col-md-4 d-flex align-items-center mx-auto">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 contenedor mx-auto" id="f2">
+    <div class="col-md-4 d-flex align-items-center mx-auto text-left">
         <span class="text-muted">&copy; 2021 Company, Inc</span>
     </div>
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex mx-auto">
-        <li class="ms-3">
-            <a href="https://twitter.com/?lang=es" target="_blank">
-                <i class="fa-brands fa-twitter color-terciario"></i>
-            </a>
-        </li>
-        <li class="ms-3">
-            <a href="https://www.instagram.com/" target="_blank">
-                <i class="fa-brands fa-instagram color-terciario"></i>
-            </a>
-        </li>
-        <li class="ms-3">
-            <a href="https://es-es.facebook.com/" target="_blank">
-                <i class="fa-brands fa-facebook color-terciario"></i>
-            </a>
-        </li>
-    </ul>
+    <div class="d-flex">
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex mx-auto">
+            <li class="ms-3">
+                <a href="https://twitter.com/?lang=es" target="_blank">
+                    <i class="fa-brands fa-twitter color-terciario"></i>
+                </a>
+            </li>
+            <li class="ms-3">
+                <a href="https://www.instagram.com/" target="_blank">
+                    <i class="fa-brands fa-instagram color-terciario"></i>
+                </a>
+            </li>
+            <li class="ms-3">
+                <a href="https://es-es.facebook.com/" target="_blank">
+                    <i class="fa-brands fa-facebook color-terciario"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
 </footer>
 
 </html>
