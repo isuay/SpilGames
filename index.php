4ab9be48dd9af89ff8447b929c9234917e6b0d42
isuay <irisau2@gmail.com>
2022-05-25 18:48:43 +0200

    Empezado memoria

--- index.php
+++ index.php
@@ -14,25 +14,25 @@
     <!-- Presentacion de diapositivas -->
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="img/slider/fondoBueno.jpg" alt="Bienvenida" class="d-block" style="width:100%">
+            <img src="img/slider/fondoBueno.jpg" alt="Bienvenida" class="d-block">
             <div id="texto-medio" class="carousel-caption">
                 <h3>¡BIENVENIDOS A SPIL GAMES, LA MEJOR PÁGINA DE JUEGOS DE CARTAS!</h3>
             </div>
         </div>
         <div class="carousel-item ">
-            <img src="img/slider/foto1.jpg" alt="Uno" class="d-block" style="width:100%">
+            <img src="img/slider/foto1.jpg" alt="Uno" class="d-block">
             <div class="carousel-caption">
                 <h3>UNO</h3>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="img/slider/foto2.jpg" alt="Spider Solitaire" class="d-block" style="width:100%">
+            <img src="img/slider/foto2.jpg" alt="Spider Solitaire" class="d-block">
             <div class="carousel-caption">
                 <h3>SPIDER SOLITAIRE</h3>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="img/slider/foto3.jpg" alt="Memory game" class="d-block" style="width:100%">
+            <img src="img/slider/foto3.jpg" alt="Memory game" class="d-block">
             <div class="carousel-caption">
                 <h3>MEMORY GAME</h3>
             </div>
