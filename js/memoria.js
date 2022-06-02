8537981732cba61dd7cb6d45ed87d591ff5912c3
isuay <irisau2@gmail.com>
2022-06-03 01:47:25 +0200

    Actualizada función reiniciar de memoria.js

--- js/memoria.js
+++ js/memoria.js
@@ -1,5 +1,6 @@
 // document.addEventListener('DOMContentLoaded', () => {
 
+// Array con las imágenes del juego
 cards = [
     "AlianzaRebelde.png",
     "AlianzaRebelde.png",
@@ -21,19 +22,52 @@ cards = [
     "Stormtrooper.png",
 ];
 
-var arrayComparar = [];
-var arrayEliminar = [];
-var contadorMov = 0;
-var contadorFin = 0;
-
-
-// NodeList con una lista de todos los elementos que contienen la clase carta
+// Guarda un NodeList - lista de todos los elementos que contienen la clase carta
 const cartaSeleccionada = document.querySelectorAll('.carta');
 
+var arrayComparar = []; // Array que guarda el src de las 2 cartas a comparar
+var arrayEliminar = []; // Array que guarda el elemento al que le quitaremos la clase flipped
+var contadorMov = 0; // Contador de movimientos
+var contadorFin = 0; // Contador de parejas encontradas para acabar el juego
+
+
+// Funcion para barajar el array de cartas y salgan aleatorias
+function random_card(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i]; // Guarda la carta en posición i
+        array[i] = array[j]; // Cambia la carta en posición i por otra de una posición aleatoria
+        array[j] = temp; // Cambia la carta de la posición aleatoria por la que había en la posición i
+        // console.log("Carta: " + temp);
+        // console.log("array " + i + ": " + array[i]);
+        // console.log("array " + j + ": " + array[j]);
+        // console.log(" ");
+    }
+    return array;
+}
 
 empezarJuego();
 
-// Verificancion de clicks
+// Funcion para imprimir las cartas aleatorias al inicio del juego
+function empezarJuego() {
+
+    // NodeList con una lista de todos los elementos que contienen la clase reverso
+    const casillas = document.querySelectorAll(".reverso");
+
+    // Usamos la función para mezclar las imágenes y las guardamos en otro array
+    var array = random_card(cards);
+
+    // Imprimimos las imágenes en la parte de atrás de cada carta
+    for (var i = 0; i < casillas.length; i++) {
+        var img = document.createElement("img");
+        img.src = "img/memoria/" + array[i];
+        casillas[i].appendChild(img);
+    }
+
+    clicked();
+}
+
+// Verificación de clicks
 function clicked() {
     cartaSeleccionada.forEach(sel => sel.addEventListener('click', event => {
         console.log('He hecho click');
@@ -53,63 +87,28 @@ function clicked() {
     }));
 }
 
-clicked();
-// Funcion para barajar el array de cartas y salgan aleatorias
-function random_card(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i]; // Guarda la carta en posición i
-        array[i] = array[j]; // Cambia la carta en posición i por otra de una posición aleatoria
-        array[j] = temp; // Cambia la carta de la posición aleatoria por la que había en la posición i
-        // console.log("Carta: " + temp);
-        // console.log("array " + i + ": " + array[i]);
-        // console.log("array " + j + ": " + array[j]);
-        // console.log(" ");
-    }
-    return array;
-}
 
-
-function empezarJuego() {
-
-    // NodeList con una lista de todos los elementos que contienen la clase reverso
-    const casillas = document.querySelectorAll(".reverso");
-
-    // Usamos la función para mezclar las imágenes de nuestro array
-    var array = random_card(cards);
-
-    // Imprimimos las imágenes en la parte de atrás de cada carta
-    for (var i = 0; i < casillas.length; i++) {
-        var img = document.createElement("img");
-        img.src = "img/memoria/" + array[i];
-        casillas[i].appendChild(img);
-    }
-}
-
+// Función para comparar si dos cartas son iguales
 function comparar() {
     // cartaSeleccionada.removeEventListener('click', clicked);
     setTimeout(function () {
         if (arrayComparar.length === 2) {
             if (arrayComparar[0] !== arrayComparar[1]) {
                 console.log('No son iguales');
-                // console.log(hijo1);
-                // console.log(hijo);
                 arrayEliminar[0].classList.remove("flipped");
                 arrayEliminar[1].classList.remove("flipped");
-                contadorMov++;
-                arrayComparar = [];
-                arrayEliminar = [];
             } else {
-                contadorMov++;
                 contadorFin += 2;
-                arrayComparar = [];
-                arrayEliminar = [];
-                ganar();
             }
+            contadorMov++;
+            arrayComparar = [];
+            arrayEliminar = [];
+            ganar();
         }
-    }, 2000)
+    }, 1500);
 }
 
+// Función para determinar si se ha ganado o no
 function ganar() {
     if (contadorFin === 18) {
         alert('¡Enhorabuena! Has acabado el juego con ' + contadorMov + ' movimientos');
@@ -117,18 +116,26 @@ function ganar() {
     }
 }
 
+// Función para reiniciar el juego
 function reiniciar() {
     cartaSeleccionada.forEach(sel => {
         const hijo = sel.firstElementChild;
         if (hijo.classList.contains("flipped")) {
             hijo.classList.remove("flipped");
-            hijo.lastElementChild.firstElementChild.remove();
         }
     });
-    checkArray = [];
-    idArray = [];
-    contador = 0;
-    fin = 0;
+    setTimeout(function() {
+        cartaSeleccionada.forEach(sel => {
+            const hijo = sel.firstElementChild;
+            if (!hijo.classList.contains("flipped")) {
+                hijo.lastElementChild.firstElementChild.remove();
+            }
+        });
+    }, 200);
+    arrayComparar = [];
+    arrayEliminar = [];
+    contadorMov = 0;
+    contadorFin = 0;
     empezarJuego();
 }
     // });
