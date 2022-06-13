8c90fb8aa9fb1b15cb121da334e622439e45f43e
isuay <irisau2@gmail.com>
2022-06-13 11:08:37 +0200

    Actualizado contacto

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,8 @@
     <meta name="description" content="HTML5, CSS3">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
@@ -73,10 +74,6 @@
                         <li><a href="./memoria/memoria.html" class="dropdown-item juegosOffCanvas">MEMORY</a></li>
                     </ul>
                 </li>
-                <!-- <li class="nav-item"><a href="#" class="nav-link">GAMES</a></li>
-                <li class="nav-item"><a href="./solitario.html" class="nav-link juegosOffCanvas">SOLITAIRE</a></li>
-                <li class="nav-item"><a href="./uno.html" class="nav-link juegosOffCanvas"></a></li>
-                <li class="nav-item"><a href="./memoria.html" class="nav-link juegosOffCanvas">MEMORY</a></li> -->
                 <li class="nav-item"><a href="#" class="nav-link">EMULATOR</a></li>
                 <li class="nav-item"><a href="./contacto.php" class="nav-link">CONTACT</a></li>
             </ul>
